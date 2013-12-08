c60e2788aac5a57d26f9d6919a72e23bd2185fd2
Kozaki, Tsuneaki <kozaki.tsuneaki@gmail.com>
2013-12-08 10:11:54 +0900

    サインインページで失敗した場合の処理を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,25 @@
+# -*- coding: utf-8 -*-
 class SessionsController < ApplicationController
   def new
   end
 
   def create
+    user = User.find_by(email: session_params[:email])
+    if user && user.authenticate(session_params[:password])
+    else
+      flash.now[:error] = 'ユーザ名またはパスワードが不正です'
+      render 'new'
+    end
   end
 
   def destroy
+  end
+
+  private
+  def session_params
+    {
+      email: params[:session][:email].downcase,
+      password: params[:session][:password]
+    }
   end
 end
--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -9,5 +9,33 @@ describe "認証機能" do
 
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
+
+    context "何も入力せずにサインイン" do
+      before { click_button 'Sign in' }
+
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error', text: '不正') }
+
+      context "エラーの後ホーム画面に戻る" do
+        before { click_link 'Home' }
+        it "フラッシュメッセージが消える" do
+          expect(page).not_to have_selector('div.alert.alert-error', text: '不正')
+        end
+      end
+    end
+
+    context "正規のユーザでサインイン" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in 'Email',    with: user.email
+        fill_in 'Password', with: user.password
+        click_button 'Sign in'
+      end
+
+      it { should have_title(user.name) }
+      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Sing out', href: signout_path) }
+      it { should_not have_link('Sign in') }
+    end
   end
 end
